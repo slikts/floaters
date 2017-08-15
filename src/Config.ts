53a82a8e0f45a368b39876ef51c0310c29ccf794
slikts <dabas@untu.ms>
2017-08-15 17:02:56 +0300

    Fix types

--- src/Config.ts
+++ src/Config.ts
@@ -3,22 +3,26 @@ import {
   multiConcat,
 } from './util'
 
-export type Config = {
+type RawConfig = {
   sceneSize: [number, number],
   planeNumber: number,
   floatersPerPlane: number,
-  blendMode: 'NORMAL' | 'ADD' | 'MULTIPLY' | 'SCREEN' | 'OVERLAY' | 'DARKEN' | 'LIGHTEN' | 'COLOR_DODGE' | 'COLOR_BURN' | 'HARD_LIGHT' | 'SOFT_LIGHT' | 'DIFFERENCE' | 'EXCLUSION' | 'HUE' | 'SATURATION' | 'COLOR' | 'LUMINOSITY' | 'NORMAL_NPM' | 'ADD_NPM' | 'SCREEN_NPM',
+  blendModeKey: 'NORMAL' | 'ADD' | 'MULTIPLY' | 'SCREEN' | 'OVERLAY' | 'DARKEN' | 'LIGHTEN' | 'COLOR_DODGE' | 'COLOR_BURN' | 'HARD_LIGHT' | 'SOFT_LIGHT' | 'DIFFERENCE' | 'EXCLUSION' | 'HUE' | 'SATURATION' | 'COLOR' | 'LUMINOSITY' | 'NORMAL_NPM' | 'ADD_NPM' | 'SCREEN_NPM',
   trailDelay: number,
   curveFactor: number,
+}
+
+export interface Config extends RawConfig {
+  blendMode: number,
   imageNames: string[],
 }
 
 const imageNums = [3, 6, 9, 12, 15, 18, 23, 30, 33, 38, 41, 45, 49, 52]
 
 export const fetchConfig = async () => {
-  const config = <Config>await (await fetch('./config.json?' + Math.random())).json()
-  return Object.assign(config, {
+  const config = <RawConfig>await (await fetch('./config.json?' + Math.random())).json()
+  return <Config>Object.assign(config, {
     imageNames: multiConcat(Math.ceil(config.floatersPerPlane / imageNums.length), imageNums).slice(0, config.floatersPerPlane).map(n => `images/slices_${n}.png`),
-    blendMode: PIXI.BLEND_MODES[config.blendMode],
+    blendMode: PIXI.BLEND_MODES[config.blendModeKey],
   })
 }
